d504521b1135429f7cbe922454244cc2c849528c
VCarames <vcarames-dev@outlook.com>
2025-04-17 21:00:45 -0700

    Hero Section Created

---
+++ src/sections/HeroSection.jsx
@@ -0,0 +1,9 @@
+function HeroSection({ heading }) {
+  return (
+    <section className="hero-section">
+      <h1 className="hero-section__heading">{heading}</h1>
+    </section>
+  );
+}
+
+export default HeroSection;
